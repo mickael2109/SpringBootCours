b5c3bfb8400efcc3559909248461da185234dc5a
Mickael <mickaelrkt20@gmail.com>
2024-03-02 10:31:31 +0300

    Code 03_03_2024

--- src/main/java/com/example/SpringProjects/resource/UserRessource.java
+++ src/main/java/com/example/SpringProjects/resource/UserRessource.java
@@ -3,11 +3,23 @@ package com.example.SpringProjects.resource;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.SpringProjects.service.UserDaoService;
+import com.example.SpringProjects.model.User;
 
 public class UserRessource {
     
+    private UserDaoService service;
+
+
     @GetMapping(path="/users")
-    public List<Users> trouverTousUser(){
-        
+    public List<User> trouverTousUser(){
+        return service.afficherTout();
+    }
+
+    @GetMapping(path = "/users/{id}")
+    public User chercherUser(@PathVariable int id){
+        return service.TrouverUser(id);
     }
 }
